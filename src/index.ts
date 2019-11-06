11ec54886b860dc1e0ee7e27c18c6d4c32fe7690
Nick Thurow <nicholas.thurow@optum.com>
2019-11-06 14:04:15 -0600

    Get something actually working

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import {join} from 'path';
 
 import {signal} from './signal';
 
-const file = readFileSync(join(__dirname, '../signal-2019-11-06-10-36-45.backup'));
+const file = readFileSync(join(__dirname, '../../signal-2019-11-06-13-00-54.backup'));
+const headerLengthBytes = file.slice(0, 4);
+const headerLength = headerLengthBytes.readUIntBE(0, 4);
+const headerFrame = file.slice(4, 4 + headerLength);
 
-const backupFrame = signal.BackupFrame.fromObject(file);
+const backupFrame = signal.BackupFrame.decode(headerFrame);
 
 if (backupFrame) {
   if (backupFrame.header) {
-    console.log(backupFrame.header.iv);
+    console.log('IV value: ', backupFrame.header.iv);
   } else {
     console.log('No header found');
   }
