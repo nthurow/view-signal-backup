197aa159bd3dbab94b8d152c4b61558ea181dc07
Nick Thurow <nicholas.thurow@optum.com>
2019-11-19 20:02:37 -0600

    Add go file to test signal backup code

--- go/backupKey.go
+++ go/backupKey.go
@@ -1,31 +1,33 @@
 package main
 
 import (
-    "fmt"
-	"crypto"
-	_ "crypto/sha512"
-	"strings"
+  "fmt"
+  "crypto"
+  _ "crypto/sha512"
+  "encoding/hex"
+  "strings"
 )
 
 func backupKey(password string, salt []byte) []byte {
-	digest := crypto.SHA512.New()
-	input := []byte(strings.Replace(strings.TrimSpace(password), " ", "", -1))
-	hash := input
+  digest := crypto.SHA512.New()
+  input := []byte(strings.Replace(strings.TrimSpace(password), " ", "", -1))
+  hash := input
 
-	if salt != nil {
-		digest.Write(salt)
-	}
+  if salt != nil {
+    digest.Write(salt)
+  }
 
-	for i := 0; i < 250000; i++ {
-		digest.Write(hash)
-		digest.Write(input)
-		hash = digest.Sum(nil)
-		digest.Reset()
-	}
+  for i := 0; i < 250000; i++ {
+    digest.Write(hash)
+    digest.Write(input)
+    hash = digest.Sum(nil)
+    digest.Reset()
+  }
 
-	return hash[:32]
+  return hash[:32]
 }
 
 func main() {
-  fmt.Println(backupKey("password", nil))
+  key := backupKey("password", nil)
+  fmt.Println(hex.EncodeToString(key))
 }
