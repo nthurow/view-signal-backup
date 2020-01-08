71b1505441ef29b4f3c39fe6e24f1839701c2a3c
Nick Thurow <nthurow@gmail.com>
2020-01-07 21:18:44 -0600

    Change frame offset AND GET INITIAL DECRYPTION WORKING

--- src/frame.ts
+++ src/frame.ts
@@ -4,7 +4,7 @@ export function nextFrameBytes(file: Buffer, offset: number) {
   const frameLengthBuffer = file.slice(offset, offset + 4);
   const frameLength = frameLengthBuffer.readUIntBE(0, 4);
   const frame = file.slice(offset + 4, offset + 4 + frameLength);
-  const newOffset = offset + 4 + frameLength + 1;
+  const newOffset = offset + 4 + frameLength;
   const isDone = newOffset >= +file.byteLength;
 
   return [frame, newOffset, isDone] as const;
--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,13 @@ import {decodeBytesToFrame, nextFrameBytes} from './frame';
 import {deriveKeyFromPassword, deriveAesKeys, decrypt} from './crypto';
 
 const file = readFileSync(join(__dirname, '../../signal-2019-11-06-13-00-54.backup'));
-//const password = '773833201388556275077355922518';
+const password = '773833201388556275077355922518';
 
 let [backupFrameBytes, newOffset] = nextFrameBytes(file, 0);
 const backupFrame = decodeBytesToFrame(backupFrameBytes);
+
+console.log('Backup frame bytes begin: ', backupFrameBytes.slice(0, 10));
+console.log('Backup frame bytes end: ', backupFrameBytes.slice(backupFrameBytes.length - 10, backupFrameBytes.length));
 
 async function getBaseKey() {
   if (!backupFrame.header || !backupFrame.header.salt) {
@@ -20,23 +23,32 @@ async function getBaseKey() {
     throw new Error('IV value not found');
   }
 
-  //const baseKey = await deriveKeyFromPassword(password, backupFrame.header.salt);
-  const baseKey = await deriveKeyFromPassword('password');
+  const baseKey = await deriveKeyFromPassword(password, backupFrame.header.salt);
+  //const baseKey = await deriveKeyFromPassword('password');
   const derivedKey = await deriveAesKeys(baseKey, 'Backup Export');
 
   const cipherKey = derivedKey.slice(0, 32);
+
+  console.log(
+    'The base key is ',
+    Buffer.from(baseKey).map((num, index) => Buffer.from(baseKey).readUInt8(index))
+  );
+  console.log('The derived key is', derivedKey);
+  console.log('The cipher key is', cipherKey);
+
   //const macKey = derivedKey.slice(32);
   //const mac = createHmac('sha256', macKey);
   //const counter = Buffer.from(backupFrame.header.iv).readUInt32BE(0);
   let [nextFrame] = nextFrameBytes(file, newOffset);
 
-  decrypt(nextFrame.slice(0, nextFrame.length - 10), 0, cipherKey, backupFrame.header.iv, (decrypted) => {
-    //const decryptedFrame = decodeBytesToFrame(decrypted);
-    //console.log(decryptedFrame);
-  });
+  console.log('Next frame length: ', nextFrame.length);
+  console.log('Next frame bytes begin: ', nextFrame.slice(0, 10));
+  console.log('Next frame bytes end: ', nextFrame.slice(nextFrame.length - 10, nextFrame.length));
 
-  console.log('The base key is ', Buffer.from(baseKey).map((num, index) => Buffer.from(baseKey).readUInt8(index)));
-  //console.log('The derived key is', derivedKey);
+  decrypt(nextFrame.slice(0, nextFrame.length - 10), 0, cipherKey, backupFrame.header.iv, (decrypted) => {
+    const decryptedFrame = decodeBytesToFrame(decrypted);
+    console.log(decryptedFrame);
+  });
 }
 
 getBaseKey();
